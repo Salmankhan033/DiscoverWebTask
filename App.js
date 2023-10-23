e5e41f605e5e4c9892ae7558cf76a01f2150e71a
Salman <salman@Salmans-MacBook-Pro.local>
2023-10-23 22:33:37 +0500

    project setup

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { PaperProvider } from 'react-native-paper';
+
 
 export default function App() {
   return (
+    <PaperProvider>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.myText}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </PaperProvider>
   );
 }
 
@@ -17,4 +22,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  myText: {
+    fontSize: hp('5%'), 
+    height: hp('70%'),
+    width: wp('8%'), 
+    backgroundColor:"red"
+  }
 });
